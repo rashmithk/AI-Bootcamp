d610fa0db40539f5147e306a48317249c9889d6c
rashmithk <rashmithkanchan@gmail.com>
2026-07-09 16:20:31 +0400

    Personal Profile.py

---
+++ week01/Day02/Personal Profile.py
@@ -0,0 +1,12 @@
+name = "John"
+age = 25
+city = "USA"
+profession = "IT Engineer"
+print("=======================")
+print("PERSON PROFILE")
+print("=======================")
+print(f"{'Name':<10} : {name}")
+print(f"{'Age':<10} : {age}")
+print(f"{'City':<10} : {city}")
+print(f"{'Profession':<10} : {profession}")
+print("=======================")
